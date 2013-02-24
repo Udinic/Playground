8df745862f1e9cc791a95a15ce498c0fb3cf2306
Udi Cohen <udinic@gmail.com>
2013-02-24 14:12:36 +0200

    update

--- ICS-Testing/src/com/udinic/ics_testing/CalendarService.java
+++ ICS-Testing/src/com/udinic/ics_testing/CalendarService.java
@@ -1,5 +1,9 @@
 package com.udinic.ics_testing;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Udic
@@ -7,5 +11,19 @@ package com.udinic.ics_testing;
  * Time: 20:13
  * To change this template use File | Settings | File Templates.
  */
-public class CalendarService {
+public class CalendarService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
