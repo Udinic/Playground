7bf417ab2076bcdc26be0cde4a7325e91a99fe5e
Udi Cohen <udinic@gmail.com>
2013-02-24 14:12:12 +0200

    udini

---
+++ ICS-Testing/src/com/udinic/ics_testing/CalendarService.java
@@ -0,0 +1,11 @@
+package com.udinic.ics_testing;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Udic
+ * Date: 21/02/13
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class CalendarService {
+}
